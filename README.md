88a5771715e20822300a4d00f8cbb42a0e72e8a3
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-01 19:07:58 +0000

    #6: Extend dinamically venv

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Flower Classifier 🍃
 
+#### Status
+[![PIPELINE status](https://github.com/omonimus1/plant_classification/actions/workflows/django.yml/badge.svg)](https://github.com/Potatoapp/backend-and-services-potato/actions/workflows/django.yml)
+
 The current repository hosts Davide Pollicino Honours Project.
 The project (still under implementation),
 offers research documentation related the implementation of a **Flower Classifier**.
 
-### CNN detailes
+### CNN details
 * The CNN implements has been build using the technologies listed below.
 So far, it is possible to classify the following categories of flowers: Tulip, Sunflower, Rose, dandelion, daisy.
 
--- webAppClassifier/webAppClassifier/settings.py
+++ webAppClassifier/webAppClassifier/settings.py
@@ -12,7 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import re
+import environ
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,11 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_l=2_dy=i8!%o$-)3!^29-t=c6c^n-k61_mjqfxp7w3qz4ec!3"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
