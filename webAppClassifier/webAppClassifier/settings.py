d850a287a5ec10c6cc3e6d50487bdf1d1450efba
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-02 12:58:28 +0000

    #5 fix .env
    Signed-off-by: Davide Pollicino <davidepollicino2015@gmail.com>

--- webAppClassifier/webAppClassifier/settings.py
+++ webAppClassifier/webAppClassifier/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 
 
 # Application definition
