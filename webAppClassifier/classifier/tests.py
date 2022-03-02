7e1e4f520e63a955645ce2944d186df22309f28b
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-02 14:53:02 +0000

    #16 Adds working Unit test on Model
    Signed-off-by: Davide Pollicino <davidepollicino2015@gmail.com>

--- webAppClassifier/classifier/tests.py
+++ webAppClassifier/classifier/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
 
+from .models import UserContactRequest
+
 
 class PredictionModelTest(TestCase):
     def test(self):
         self.assertEquals(200, 200)
+
+
+class UserContactTest(TestCase):
+    @classmethod
+    def setUp(self):
+        UserContactRequest.objects.create(name='Davide', message='Hi, is prediction working?')
+
+    def testSupportUserName(self):
+        userRequest = UserContactRequest.objects.get(pk=1)
+        self.assertEquals(userRequest.name, 'Davide')
 
 
 """
