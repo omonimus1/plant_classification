6182071c2fcfa65bb8cc7d06a3ac51e75e8fb78d
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-02 14:15:37 +0000

    #16 Introduce requirements
    Signed-off-by: Davide Pollicino <davidepollicino2015@gmail.com>

--- webAppClassifier/classifier/tests.py
+++ webAppClassifier/classifier/tests.py
@@ -1,6 +1,25 @@
 from django.test import TestCase
 
 
-class AnimalTestCase(TestCase):
-    def test_animals_can_speak(self):
-        self.assertEqual("hello", "hello")
+class PredictionModelTest(TestCase):
+    def test(self):
+        self.assertEquals(200, 200)
+
+
+"""
+class PredictionModelTest(TestCase):
+    @classmethod
+    def setUp(cls):
+        current_directory = os.getcwd()
+        image_path = current_directory + "testImages/sunflower.jpeg"
+
+        newPhoto = SimpleUploadedFile(name='test_image.jpg', content=open(image_path, 'rb').read(),
+                                            content_type='image/jpeg')
+        Prediction.objects.create(name='Python', image=newPhoto.image)
+
+        def test_name_max_length(self):
+            unit = Prediction.objects.get(id=1)
+            max_length = unit._meta.get_field('name').max_length
+            self.assertEquals(max_length, 200)
+
+"""
