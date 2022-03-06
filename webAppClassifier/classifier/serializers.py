f0d5d45cc3f20e89125f07a0f8040146992ab96e
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-06 15:45:50 +0000

    Adds thank you page

---
+++ webAppClassifier/classifier/serializers.py
@@ -0,0 +1,12 @@
+"""
+from .models import Prediction
+from django.db import models
+
+
+class PredictionSerializer(serializers.ModelSerializer):
+    image = models.ImageField(upload_to="img/%Y/%m/%d/", blank=False)
+
+    class Meta:
+        model = Prediction
+        fields = "image"
+""
