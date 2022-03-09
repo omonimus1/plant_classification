93707a9d96be26bce88767426120a234d98947f2
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-09 00:16:52 +0000

    Improve settings

--- webAppClassifier/webAppClassifier/settings.py
+++ webAppClassifier/webAppClassifier/settings.py
@@ -14,20 +14,14 @@ from pathlib import Path
 import os
 import environ
 
-# Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
+
 ALLOWED_HOSTS = []
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Application definition
