ab210a054393e322ad7bc2f0510d804895ceb891
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-10 16:50:26 +0000

    Creates Favourite api #28

--- webAppClassifier/classifier/models.py
+++ webAppClassifier/classifier/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime    
 
 
 class Prediction(models.Model):
     name = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to="img/%Y/%m/%d/", blank=False)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.name + ' ' + str(self.pk)
@@ -15,6 +17,8 @@ class Result(models.Model):
     expected_result = models.CharField(
         max_length=200, blank=True, null=False, default=""
     )
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
 
     def __str__(self):
         return self.expected_result
@@ -23,6 +27,7 @@ class Result(models.Model):
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     prediction = models.ForeignKey(Prediction, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.user.username
