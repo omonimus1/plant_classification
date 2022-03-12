fab497f4d56c34f607dc8b8e34b8ef01d1f8ed86
Davide Pollicino <davidepollicino2015@gmail.com>
2022-03-12 15:41:32 +0000

    Adds favorite test

--- webAppClassifier/classifier/tests.py
+++ webAppClassifier/classifier/tests.py
@@ -3,16 +3,16 @@ from django.test import RequestFactory, TestCase
 from django.core import mail
 from .views import Index, ImageView
 from django.contrib.auth.models import User
-from .models import Prediction
+from .models import Prediction, Result, Favorite
 from PIL import Image
 import io
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 def create_image():
-    image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
+    image = Image.new("RGBA", size=(50, 50), color=(256, 0, 0))
     image_file = io.BytesIO(image.tobytes("hex", "rgb"))
-    file = InMemoryUploadedFile(image_file, None, 'test.jpg', 'image/jpg', 1024, None)
+    file = InMemoryUploadedFile(image_file, None, "test.jpg", "image/jpg", 1024, None)
     return file
 
 
@@ -38,7 +38,7 @@ class IndexViewTestCase(TestCase):
 
 class ViewsTestCase(TestCase):
     def test_index_loads_properly(self):
-        response = self.client.get('http://127.0.0.1:8000/')
+        response = self.client.get("http://127.0.0.1:8000/")
         self.assertEqual(response.status_code, 200)
 
 
@@ -58,27 +58,76 @@ class EmailTest(TestCase):
 class UserModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User.objects.create(first_name='davide', last_name='pollicino',
-                            username='davidetest', email='davide@test.com', password='123456'
-                            )
+        User.objects.create(
+            first_name="davide",
+            last_name="pollicino",
+            username="davidetest",
+            email="davide@test.com",
+            password="123456",
+        )
 
     def test_name_user(self):
         user = User.objects.get(id=1)
-        self.assertEquals(user.first_name, 'davide')
-        self.assertEquals(user.last_name, 'pollicino')
-        self.assertEquals(user.username, 'davidetest')
-        self.assertEquals(user.email, 'davide@test.com')
+        self.assertEquals(user.first_name, "davide")
+        self.assertEquals(user.last_name, "pollicino")
+        self.assertEquals(user.username, "davidetest")
+        self.assertEquals(user.email, "davide@test.com")
 
 
 class PredictionModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
+        image = Image.new("RGBA", size=(50, 50), color=(256, 0, 0))
         image_file = io.BytesIO(image.tobytes("hex", "rgb"))
-        file = InMemoryUploadedFile(image_file, None, 'test.jpg', 'image/jpg', 1024, None)
-        p = Prediction.objects.create(image=file, name='rose')
+        file = InMemoryUploadedFile(
+            image_file, None, "test.jpg", "image/jpg", 1024, None
+        )
+        p = Prediction.objects.create(image=file, name="rose")
         p.save()
 
     def test_prediction_record(self):
         prediction = Prediction.objects.get(pk=1)
-        self.assertEquals(prediction.name, 'rose')
+        self.assertEquals(prediction.name, "rose")
+
+
+class ResultModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        file = create_image()
+        p = Prediction.objects.create(image=file, name="rose")
+        p.save()
+        result = Result.objects.create(
+            image=p, expected_result="I was expecting a rose"
+        )
+        result.save()
+
+    def test_feedback_record(self):
+        result = Result.objects.get(pk=1)
+        self.assertEquals(result.image.name, "rose")
+        self.assertEquals(result.expected_result, "I was expecting a rose")
+        max_length = result._meta.get_field("expected_result").max_length
+        self.assertEquals(max_length, 200)
+
+
+class FavoriteModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        file = create_image()
+        p = Prediction.objects.create(image=file, name="rose")
+        p.save()
+        user = User.objects.create(
+            first_name="davide",
+            last_name="pollicino",
+            username="davide",
+            email="davide@test.com",
+            password="123456",
+        )
+        user.save()
+        favorite = Favorite.objects.create(user=user, prediction=p)
+        favorite.save()
+
+    def test_feedback_record(self):
+        favorite = Favorite.objects.get(pk=1)
+        self.assertEquals(favorite.user.username, "davide")
+        self.assertEquals(favorite.prediction.name, "rose")
+        self.assertEquals(favorite.user.first_name, "davide")
